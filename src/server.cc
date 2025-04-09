01bd122ec47d5062d4e051c4443b9c721fb4081a
Yuheng Ding <yhding@g.ucla.edu>
2025-04-09 05:35:59 +0000

    refactor server_main to separate files
    Change-Id: Ia605d0b5f1b90836835963ea3b2c7f633405d1b3

---
+++ src/server.cc
@@ -0,0 +1,47 @@
+// A server class that accepts incoming connections and starts a session for each client.
+//
+// Adopted from the Boost Asio example: async_tcp_echo_server.cpp
+// ~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+// Copyright (c) 2003-2017 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+#include <iostream>
+#include "server.h"
+#include "session.h"
+#include <boost/asio.hpp>
+#include <boost/bind/bind.hpp>
+
+using boost::asio::ip::tcp;
+
+server::server(boost::asio::io_service &io_service, short port)
+    : io_service_(io_service),
+      acceptor_(io_service, tcp::endpoint(tcp::v4(), port))
+{
+    start_accept();
+}
+
+void server::start_accept()
+{
+    session *new_session = new session(io_service_);
+    acceptor_.async_accept(new_session->socket(),
+                           boost::bind(&server::handle_accept, this, new_session,
+                                       boost::asio::placeholders::error));
+}
+
+void server::handle_accept(session *new_session,
+                   const boost::system::error_code &error)
+{
+    if (!error)
+    {
+        new_session->start();
+    }
+    else
+    {
+        delete new_session;
+    }
+
+    start_accept();
+}

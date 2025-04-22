e3d83106ff32b64e3a503f0aa5cfcb3509e4ff78
YUHENG DING <yhding@g.ucla.edu>
2025-04-22 00:30:35 +0000

    Revert "Change CodeCoverageReport Config; Removed Intergration test report"
    This reverts commit 609017f557986fe8beb4defcf5ec626ffc52d1fe.
    
    Reason for revert: <Google Cloud Build Failure>
    
    Change-Id: I8cea356230e440b7e6ce6c4409403a1260812f04

--- cmake/CodeCoverageReportConfig.cmake
+++ cmake/CodeCoverageReportConfig.cmake
@@ -13,17 +13,27 @@ find_program(GCOVR_PATH gcovr)
 # report
 #
 # generate_coverage_report(
+#     NAME    <custom_target_name>   # optional, default "coverage"
+#     LABEL   unit                   # ctest -L <label>     (optional)
 #     TARGETS [targets...]
 #     TESTS [test_targets...]
 # )
 function(generate_coverage_report)
     # Parse arguments
+    set(oneValueArgs NAME LABEL)
     set(multiValueArgs TARGETS TESTS)
-    cmake_parse_arguments(COVERAGE "" "" "${multiValueArgs}" ${ARGN})
+    cmake_parse_arguments(COVERAGE "" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
-    set(COVERAGE_REPORT_DIR "${CMAKE_BINARY_DIR}/report")
-    set(COVERAGE_DATA_STAMP_FILE "${CMAKE_BINARY_DIR}/coverage_data.stamp")
-    set(COVERAGE_REPORT_FILE "${COVERAGE_REPORT_DIR}/index.html")
+    # ---------- Derive names/paths --------------------------------------------
+    if(COVERAGE_NAME)
+        set(_name "${COVERAGE_NAME}")
+    else()
+        set(_name "coverage")          # default target
+    endif()
+
+    set(COVERAGE_REPORT_DIR   "${CMAKE_BINARY_DIR}/${_name}_report")
+    set(COVERAGE_DATA_STAMP_FILE   "${CMAKE_BINARY_DIR}/${_name}_data.stamp")
+    set(COVERAGE_REPORT_FILE  "${COVERAGE_REPORT_DIR}/index.html")
 
     if(NOT GCOVR_PATH)
         message(FATAL_ERROR "gcovr not found! Aborting...")
@@ -31,7 +41,7 @@ function(generate_coverage_report)
 
     string(TOLOWER "${CMAKE_BUILD_TYPE}" LOWER_CMAKE_BUILD_TYPE)
     if("${LOWER_CMAKE_BUILD_TYPE}" STREQUAL "coverage")
-        message(STATUS "Coverage will be measured for targets: ${COVERAGE_TARGETS}")
+        message(STATUS "${COVERAGE_NAME} will be measured for targets: ${COVERAGE_TARGETS}")
 
         # Create Coverage build type
         set(CMAKE_CXX_FLAGS_COVERAGE "${CMAKE_CXX_FLAGS_DEBUG}" PARENT_SCOPE)
@@ -50,13 +60,20 @@ function(generate_coverage_report)
             set_property(TARGET ${T} APPEND_STRING PROPERTY LINK_FLAGS " -g --coverage")
         endforeach()
 
+        # ---------------------- decide which tests to run ------------------------
+        if(COVERAGE_LABEL)
+            set(_ctest_args -L ${COVERAGE_LABEL})
+        else()
+            set(_ctest_args)               # empty → run all tests
+        endif()
+
         # Use coverage_data.stamp as a dependency proxy for coverage data files
         add_custom_command(OUTPUT ${COVERAGE_DATA_STAMP_FILE}
             COMMAND find "${CMAKE_BINARY_DIR}" -name "*.gcda" -exec rm {} \;
-            COMMAND ${CMAKE_CTEST_COMMAND}
+            COMMAND ${CMAKE_CTEST_COMMAND} ${_ctest_args}
             COMMAND touch "${COVERAGE_DATA_STAMP_FILE}"
             DEPENDS ${COVERAGE_TARGETS} ${COVERAGE_TESTS}
-            COMMENT "Running tests to generate coverage data"
+            COMMENT "Running tests to generate ${COVERAGE_NAME} data"
             VERBATIM
         )
 
@@ -69,9 +86,9 @@ function(generate_coverage_report)
             --object-directory "${CMAKE_BINARY_DIR}"
             -o "${COVERAGE_REPORT_FILE}"
             DEPENDS ${COVERAGE_DATA_STAMP_FILE}
-            COMMENT "Generating coverage report"
+            COMMENT "Generating ${COVERAGE_NAME} report"
             VERBATIM)
-        add_custom_target(coverage DEPENDS ${COVERAGE_REPORT_FILE})
+        add_custom_target(${COVERAGE_NAME} DEPENDS ${COVERAGE_REPORT_FILE})
 
         # Clean up the report directory on make clean
         set_property(DIRECTORY APPEND PROPERTY ADDITIONAL_MAKE_CLEAN_FILES ${COVERAGE_REPORT_DIR})
